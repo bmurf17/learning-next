275b16d16f3409c896ed7c45a40b6ca6745a4cfd
Brendan Murphy <brendan.murphy@ostusa.com>
2023-07-20 07:46:45 -0400

    Got a calendar in

--- components/program details/Calendar.tsx
+++ components/program details/Calendar.tsx
@@ -1,7 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { CalendarEvent, Schedulely } from "schedulely";
+import "schedulely/dist/index.css";
+
 export default function Calendar() {
+  const router = useRouter();
+  const storyEvents: CalendarEvent[] = [
+    {
+      color: "#a855f7",
+      end: "2023-07-19T16:07:22.292Z",
+      id: "f147",
+      start: "2023-07-19T16:07:22.292Z",
+      summary: "Cool Hat",
+    },
+  ];
+
   return (
-    <div className="flex flex-row align-middle justify-center mt-14">
-      Calendar Or Next Days It Comes Up
+    <div className="flex flex-row align-middle justify-center mt-14 md:m-28 m-4">
+      <Schedulely
+        dark={localStorage.getItem("theme") === "dark"}
+        events={storyEvents}
+        additionalClassNames={["w-full"]}
+        actions={{
+          onEventClick: (event) => router.push("/"),
+          onMoreEventsClick: (events) => router.push("/"),
+        }}
+      />
     </div>
   );
 }
