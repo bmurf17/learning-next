a27eae3d30a2370c908c388a19449761c9fdae71
Brendan Murphy <brendan.murphy@ostusa.com>
2023-08-14 14:07:55 -0400

    Revert "Start of auth stuff"
    This reverts commit 9a67b681cdbdbcfb6ecf297ce89460593829ea6b.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,23 +1,11 @@
-"use client";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
 
 export default function NavBar() {
   return (
-    <header className="w-full absolute z-10">
-      <div className="flex bg-purple-500 text-white p-4 justify-between">
-        <Link href={"/"}>
-          <div className="">FCPM</div>
-        </Link>
-        <div
-          onClick={() => {
-            signIn("google");
-          }}
-          className="cursor-pointer"
-        >
-          Login
-        </div>
-      </div>
+    <header className="w-full  absolute z-10">
+      <Link href={"/"}>
+        <div className="bg-purple-500 text-white p-4">FCPM</div>
+      </Link>
     </header>
   );
 }
