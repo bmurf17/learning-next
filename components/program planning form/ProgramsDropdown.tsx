3c82d90ddf7813d0986d150e8531b3cecd9c294c
Brendan Murphy <brendan.murphy@ostusa.com>
2023-09-16 11:17:59 -0400

    Fix console error

--- components/program planning form/ProgramsDropdown.tsx
+++ components/program planning form/ProgramsDropdown.tsx
@@ -26,7 +26,7 @@ export default function ProgramsDropdown({
   };
 
   return (
-    <div className="">
+    <div>
       <label className="block text-gray-700 text-sm font-bold">Program </label>
       <Listbox
         value={selected}
@@ -35,7 +35,10 @@ export default function ProgramsDropdown({
         }}
       >
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+          <Listbox.Button
+            id="listbox-button"
+            className="relative w-full cursor-default rounded bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+          >
             <span className="block truncate">{selected.name}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
